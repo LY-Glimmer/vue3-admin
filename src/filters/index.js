0e432c71badd6923fe0464b50a579ba84709c6a7
LY_glimmer <1692332546@qq.com>
2021-11-27 15:14:32 +0800

    feat: 文章排名 文章详情 创建文章 编辑文章 功能完毕

--- src/filters/index.js
+++ src/filters/index.js
@@ -5,16 +5,34 @@
  * @time: 2021.11.23
  **/
 import dayjs from 'dayjs'
+import rt from 'dayjs/plugin/relativeTime'
+// 语言包
+import 'dayjs/locale/zh-cn'
+import store from '@/store'
 // 时间过滤器
-export const dateFilter = (val, format = 'YYYY-MM-DD') => {
+const dateFilter = (val, format = 'YYYY-MM-DD') => {
   if (!isNaN(val)) {
     val = parseInt(val)
   }
+
   return dayjs(val).format(format)
 }
-// 给全局添加一个过滤器对象
+
+// 加载相对时间插件
+dayjs.extend(rt)
+
+function relativeTime (val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  return dayjs()
+    .locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+    .to(dayjs(val))
+}
+
 export default app => {
   app.config.globalProperties.$filters = {
-    dateFilter
+    dateFilter,
+    relativeTime
   }
 }
